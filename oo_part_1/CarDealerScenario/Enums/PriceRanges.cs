064e6effa656fa7e16a8030965afca60166b4047
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-02-19 18:03:16 +0100

    PriceRanges

---
+++ oo_part_1/CarDealerScenario/Enums/PriceRanges.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerScenario.Enums
+{
+    /// <summary>
+    /// Cheap - from 0 to 10 000
+    /// Mid - from 10 to 100 000
+    /// Expensive - more than 100 000
+    /// </summary>
+    internal enum PriceRanges
+    {
+        Cheap, Mid, Expensive
+    }
+}
