baca77fffdc94c98e3f34ab2f8bc01e9eb185bb5
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-02-19 17:48:17 +0100

    up

---
+++ oo_part_1/CarDealerScenario/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarDealerScenario // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ oo_part_1/CarDealerScenario/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerScenario
+{
+    internal class Vehicle
+    {
+        private long Id;
+        private decimal Price;
+        private string LicensePlate;
+
+        public Vehicle(long id, decimal price, string licensePlate)
+        {
+            Id = id;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+        public void SetPrice(decimal price)
+        {
+            this.Price = price;
+        }
+        public decimal GetPrice()
+        {
+            return this.Price;
+        }
+    }
+}
