911d76e52e1000ef244e1a01fc6970455bec6f64
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-02-22 13:42:20 +0100

    Update CarDealerTest.cs

--- oo_part_1/TestCarDealer/CarDealerTest.cs
+++ oo_part_1/TestCarDealer/CarDealerTest.cs
@@ -28,5 +28,20 @@ namespace TestCarDealer
 
             Assert.IsInstanceOfType(vehicle, typeof(Vehicle));
         }
+
+        [TestMethod]
+        public void AddVehicleTest()
+        {
+            long id = 1;
+            decimal price = 123;
+            string licensePlate = "asda";
+            var type = CarDealerScenario.VehicleType.Car;
+
+            Vehicle vehicle = this.CarDealer.CreateVehicle(id, price, licensePlate, type);
+
+            var listResult = this.CarDealer.AddVehicle(vehicle);
+
+            Assert.AreEqual(listResult[0], vehicle);
+        }
     }
 }
