c91ad913feaa0f40290994312bb203acbd26a86c
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-03-08 11:48:42 +0100

    Update User.cs

--- oo_part_2/Assignment2PPAR/Entities/User.cs
+++ oo_part_2/Assignment2PPAR/Entities/User.cs
@@ -9,16 +9,60 @@ namespace Assignment2.Entities
 {
     public abstract class User : IUser
     {
-        public string Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string StudyProgram { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Cohort { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Class { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string PhoneNumberWork { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string LecturerAbbreviation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Nationality { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public DateTime DateStarted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Type
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string FirstName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string LastName
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string Email
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string StudyProgram
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string Cohort
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string Class
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string PhoneNumberWork
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string LecturerAbbreviation
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public string Nationality
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public DateTime DateStarted
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
     }
 }
