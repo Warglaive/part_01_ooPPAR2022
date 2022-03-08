a893ebbae7e86646f88929f9e378428d07889402
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-03-08 12:45:20 +0100

    Update Lecturer.cs

--- oo_part_2/Assignment2PPAR/Entities/Lecturer.cs
+++ oo_part_2/Assignment2PPAR/Entities/Lecturer.cs
@@ -1,9 +1,4 @@
 ﻿using Assignment2.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assignment2.Entities
 {
