3a411268a20ad1db0536277cb472a4cb7ea33880
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-03-08 13:08:05 +0100

    Update StartUp.cs

--- oo_part_2/Assignment2PPAR/StartUp.cs
+++ oo_part_2/Assignment2PPAR/StartUp.cs
@@ -10,7 +10,7 @@ namespace Assignment2 // Note: actual namespace depends on the project name.
             //use reflection to get Entities data
             // User user = new();
             var type = typeof(User).Assembly.GetTypes().Where(x => x.IsSubclassOf(typeof(User)));
-            foreach (var item in type.ToList())
+            foreach (var item in type)
             {
                 Console.WriteLine(item.Name);
             }
